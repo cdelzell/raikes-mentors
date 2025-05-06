3a3aaa4da6f69248b388dd1f3588ffb930fdef20
cdelzell <claradelzell@gmail.com>
2025-05-05 23:38:51 -0500

    downloaded dependencies, MUI matierial ui and toolbox for component work

---
+++ raikes-mentors/src/signIn.tsx
@@ -0,0 +1,44 @@
+import { AppProvider } from "@toolpad/core/AppProvider";
+import {
+  SignInPage,
+  type AuthProvider,
+  type AuthResponse,
+} from "@toolpad/core/SignInPage";
+
+const providers = [
+  { id: "github", name: "GitHub" },
+  { id: "google", name: "Google" },
+  { id: "credentials", name: "Email and Password" },
+];
+
+const signIn: (provider: AuthProvider) => void | Promise<AuthResponse> = async (
+  provider
+) => {
+  const promise = new Promise<AuthResponse>((resolve) => {
+    setTimeout(() => {
+      console.log(`Sign in with ${provider.id}`);
+      resolve({ error: "This is a mock error message." });
+    }, 500);
+  });
+  return promise;
+};
+
+export default function SignIn() {
+  return (
+    // preview-start
+    <AppProvider theme={THEME}>
+      <SignInPage
+        signIn={signIn}
+        providers={providers}
+        slotProps={{ form: { noValidate: true } }}
+        sx={{
+          "& form > .MuiStack-root": {
+            marginTop: "2rem",
+            rowGap: "0.5rem",
+          },
+        }}
+      />
+    </AppProvider>
+    // preview-end
+  );
+}
